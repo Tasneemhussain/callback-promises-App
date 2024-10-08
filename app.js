bc1fb2296b398a3283bef745a8db71fffba2f61b
Tasneemhussain <tasneemhussain321@gmail.com>
2024-10-08 19:12:35 +0200

    text change

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 // Function for  coin flip
+
 const flipCoin = () => {
   return new Promise((resolve, reject) => {
     const outcome = Math.random(); // Generate a random number between 0 and 1
@@ -11,17 +12,26 @@ const flipCoin = () => {
 };
 
 // Fetch random advice when the player loses
-const RandomAdvice = () => {
-  return fetch("https://api.adviceslip.com/advice")
-    .then((response) => response.json())
-    .then((data) => data.slip.advice)
-    .catch(() => "something went worng please try again");
+const RandomAdvice = async () => {
+  try {
+    // .then and catch method
+    const response = await fetch("https://api.adviceslip.com/advice"); // return fetch("https://api.adviceslip.com/advice")
+    const data = await response.json(); //.then((response) => response.json())
+    return data.slip.advice; // Return the fetched advice                      // .then((data) => data.slip.advice)
+  } catch (error) {
+    return "something went worng please try again"; //.catch(() => "something went worng please try again");
+  }
 };
-const RandomJoke = () => {
-  return fetch("https://official-joke-api.appspot.com/random_joke")
-    .then((response) => response.json())
-    .then((data) => ` ${data.setup} - ${data.punchline}`)
-    .catch(() => "something went worng please try later");
+const RandomJoke = async () => {
+  try {
+    const response = await fetch(
+      "https://official-joke-api.appspot.com/random_joke"
+    );
+    const data = await response.json();
+    return ` ${data.setup} - ${data.punchline}`;
+  } catch (error) {
+    return "something went worng please try later";
+  }
 };
 
 // funtion for Event listener for the button
@@ -52,4 +62,31 @@ document.getElementById("flipButton").addEventListener("click", () => {
         extraMessageDiv.style.color = "red";
       }
     });
+});
+
+// weather api function
+document.getElementById("getWeather").addEventListener("click", () => {
+  const Weatherdiv = document.getElementById("Weather");
+  const searchInput = document.getElementById("searchcity").value;
+  Weatherdiv.textContent = ""; // clear previous result
+
+  const apikey = "a88af912f62bac7ec2b9e1cd525f01da";
+  const lat = 56.0467;
+  const lon = 12.6944;
+  const units = "metric";
+
+  const weatherInhelsingborg = async () => {
+    try {
+      // const response = await fetch ("https://api.open-meteo.com/v1/forecast?latitude=56.0467&longitude=12.6944&current=temperature_2m&timezone=Europe%2FBerlin");
+      const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apikey}&units=${units}`
+      );
+      const data = await response.json();
+
+      Weatherdiv.textContent = `The temperature in Helsingborg is currently: ${data.main.temp}°C`;
+    } catch (error) {
+      Weatherdiv.textContent = "something went worng. Please try again later.";
+    }
+  };
+  weatherInhelsingborg();
 });
